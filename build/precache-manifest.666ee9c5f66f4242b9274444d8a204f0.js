53313a6cc1bda2d2076e3d3ee42a71567907a32c
Nadenka11 <57888500+Nadenka11@users.noreply.github.com>
2019-11-20 01:56:31 +0200

    Add files via upload

---
+++ build/precache-manifest.666ee9c5f66f4242b9274444d8a204f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28230ffd94dc0de456b2972d53d9f00e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd41af31925eeaed918b",
+    "url": "/static/css/main.7e22d3d4.chunk.css"
+  },
+  {
+    "revision": "1775d7c1fcbb24063309",
+    "url": "/static/js/2.e83d11ba.chunk.js"
+  },
+  {
+    "revision": "cd41af31925eeaed918b",
+    "url": "/static/js/main.dc30df2a.chunk.js"
+  },
+  {
+    "revision": "d9b8fdca9571c82db174",
+    "url": "/static/js/runtime-main.765e96f6.js"
+  }
+]);
